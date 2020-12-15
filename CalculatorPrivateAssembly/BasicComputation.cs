1f53688e9f1cdbdb9c67747f3ea5b5b92fbfb0dc
Edward Enejosa <eenjosa@gmail.com>
2020-12-16 04:31:16 +0800

    Basic Calculator with Private Assembly

---
+++ CalculatorPrivateAssembly/BasicComputation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CalculatorPrivateAssembly
+{
+    public delegate float getTotal(float x, float y);
+    public class BasicComputation
+    {
+        public static float Addition(float Num1, float Num2)
+        {
+            float answer = Num1 + Num2;
+            return answer;
+        }
+        public static float Substraction(float Num1, float Num2)
+        {
+            float answer = Num1 - Num2;
+            return answer;
+        }
+        public static float Multiplication(float Num1, float Num2)
+        {
+            float answer = Num1 * Num2;
+            return answer;
+        }
+        public static float Division(float Num1, float Num2)
+        {
+            float answer = Num1 / Num2;
+            return answer;
+        }
+    }
+}
